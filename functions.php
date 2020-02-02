15236db61fd7cd8ff43f7dfda2f9a643969aca87
Jason <59642133+jasonandrades007@users.noreply.github.com>
2020-02-02 16:05:02 +1000

    Update functions.php
    The functions.php file is complete and all the relative functions are added.

--- functions.php
+++ functions.php
@@ -40,6 +40,74 @@ function include_jquery()
 }
 add_action('wp_enqueue_scrpts', 'include_jquery');
 
+function loadjs()
+{
+
+    wp_register_script('customjs', get_template_directory_uri() . '/js/scripts.js', '', 1, true );
+    wp_enqueue_script('customjs');
+
+
+}
+add_action('wp_enqueue_scrpts', 'loadjs');
+
+
+add_theme_support('menus');
+
+add_theme_support('post-thumbnails');
+
+
+register_nav_menus(
+
+    array(
+
+
+            'top-menu' =>  __('Top Menu', 'theme'),
+            'footer-menu' =>  __('Footer Menu', 'theme'),
+
+
+
+    )
+
+
+);
+ 
+function themename_custom_logo_setup() {
+    $defaults = array(
+    'height'      => 100,
+    'width'       => 400,
+    'flex-height' => true,
+    'flex-width'  => true,
+    'header-text' => array( 'site-title', 'site-description' ),
+    );
+    add_theme_support( 'custom-logo', $defaults );
+   }
+   add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
+
+   if ( function_exists( 'the_custom_logo' ) ) {
+ the_custom_logo();
+}
+
+add_action( 'after_setup_theme', 'register_multiple_widget_areas' );
+
+function register_multiple_widget_areas()
+{
+    register_sidebar(
+        array(
+        'name'          => 'Sidebar',
+        'id'            => 'sidebar',
+        'description'   => 'describe the function of the box.'
+        )
+    );
+    register_sidebar(
+        array(
+        'name'          => 'Header',
+        'id'            => 'header-widget',
+        'description'   => 'Goes at the top of the page.'
+        )
+    );
+}
+
+
 
 
 
