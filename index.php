bf97d4dd4bb7fdd48923c8f2b41b19a3eef7a6b1
Sambid Prasad Rijal <sambidrijal71@gmail.com>
2020-02-03 16:52:35 +1000

    added index.php
    added index.php to run the php theme in the website. Also added few stylings with in php file

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.hero-image {
+  background-image: url("https://www.gettyimages.com.au/detail/photo/classic-black-car-in-garage-royalty-free-image/862415786?adppopup=true");
+  background-color: #cccccc;
+  height: 500px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+
+</style>
+</head>
+<body>
+
+<div class="hero-image">
+</div>
+
+
+</body>
+</html>
