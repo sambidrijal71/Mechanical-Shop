546f194443206813ff84f47ff6f1c9e799db0074
Sambid Prasad Rijal <sambidrijal71@gmail.com>
2020-02-03 17:45:28 +1000

    code to add logo
    Added code to insert logo for the website dynamically from wordpress theme settings.

--- header.php
+++ header.php
@@ -1,3 +1,12 @@
+<?php
+ if( has_custom_logo() ) { 
+  the_custom_logo(); 
+    } else { ?>
+    <h1 class="navbar-brand mb-0"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+
+<?php } ?>
+
 <!DOCTYPE html>
  <html>
         <head>
@@ -26,7 +35,22 @@
             );?>
             </div>
 
+   
+<div id="header-image">
+    <? php the_custom_logo();?>
+    
+       <?php
+                $logo_sticky = $protocol . str_replace( array( 'http://', 'https://' ), '', $params['sticky_image']['url'] );
+             ?>
+
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="Custom-logo rel="home">
+                <?php echo '<img src="' . $logo_sticky . '" alt="' . get_bloginfo( 'name' ) . '" style="max-width: ' . esc_attr( $sticky_width ) . 'px;" />'; ?>
+            </a>
+</div>
+
+
 
 </header>
+
 
 
